44b142e0cbb83ef483b7643f166994bd50a7d95c
Valentin Bourdaud <bourdaudv@gmail.com>
2018-06-12 10:35:27 +0200

    error interceptors

--- src/app/services/error-interceptor.service.ts
+++ src/app/services/error-interceptor.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
 import "rxjs/add/operator/catch";
-import "rxjs/add/operator/throw"
+import { catchError,  } from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,10 @@ export class ErrorInterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).catch((errorResponse: HttpErrorResponse) => {
+    return next.handle(req).pipe(catchError((errorResponse: HttpErrorResponse) => {
         if (errorResponse.status === 404) {
-          // TODO: Gérer l'erreur
         }
         return Observable.throw(errorResponse);
-      });
+      }));
   }
 }
